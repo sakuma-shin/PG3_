7e5b14e2c742bc9ccb791aab1fccf89f6ea2fee1
sakuma-shin <k023g0084@g.neec.ac.jp>
2024-10-15 12:42:00 +0900

    課題終了

--- main.cpp
+++ main.cpp
@@ -1,30 +1,50 @@
-#include<stdio.h>
-#include<Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
 
-float Pay(int n,float firstPay) {
-	if (n <= 1) {
-		return firstPay;
-	}
+// コールバック関数の型を定義
+typedef void (*Callback)(int);
 
-	return Pay(n - 1, firstPay * 2.0f - 50.0f) ;
+// サイコロを振る関数（抽選結果をランダムで決める）
+int RollDice() {
+    return rand() % 6 + 1;
 }
+
+// 結果を判定し、コールバック関数を呼び出す
+void CheckResult(int result, int guess, Callback callback) {
+    // 三秒間待機
+    Sleep(3000);
+    // コールバック関数を呼び出し
+    callback(result == guess);
+}
+
+// 結果を表示するコールバック関数
+void ShowResult(int is_correct) {
+    if (is_correct) {
+        printf("当たりです\n");
+    } else {
+        printf("ハズレです\n");
+    }
+}
+
 int main() {
-	
-	
-	float pay = 100.0f;
-	float hourlyPay = 1072.0f;
-	int workTime = 8;
+    // 乱数の種を設定
+    srand(time(nullptr));
 
-	//再帰的な給料計算
-	pay=Pay(workTime, pay);
+    // サイコロの出目を決定
+    int diceResult = RollDice();
 
-	//一般的な給料計算
-	hourlyPay*=static_cast<float>(workTime);
+    // 出目の奇数・偶数を判定
+    int diceParity = diceResult % 2;
 
+    // ユーザーから奇数か偶数かの入力を受け取る
+    int userGuess;
+    printf("奇数(1)か偶数(0)かを当ててください: ");
+    scanf_s("%d", &userGuess);
 
-	printf("一般的な資金体系の%d時間の給料は%f円\n", workTime, hourlyPay);
-	printf("再帰的な資金体系の%d時間の給料は%f円\n", workTime, pay);
+    // 結果を判定し、コールバック関数を使用
+    CheckResult(diceParity, userGuess, ShowResult);
 
-	
-	return 0;
+    return 0;
 }
