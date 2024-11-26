971f26b84fe7dd3a27ab637fd6063b39eedec319
sakuma-shin <k023g0084@g.neec.ac.jp>
2024-11-26 11:33:08 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,44 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <Windows.h>
+#include <iostream>
+#include <memory>  // std::shared_ptr
 
-//サイコロを振る
-int RollDice() {
-    return rand() % 6 + 1;
-}
+// 基底クラス：Animal
+class Animal {
+public:
+    virtual void makeSound() const = 0; // 純粋仮想関数
+    virtual ~Animal() = default;
+};
 
-//結果を判定
-void CheckResult(int result, int guess, void(*callback)(int)) {
-    //三秒間待機
-    Sleep(3000);
-    //呼び出し
-    callback(result == guess);
-}
+// 派生クラス：Dog
+class Dog : public Animal {
+public:
+    void makeSound() const override {
+        std::cout << "Woof!" << std::endl;
+    }
+};
+
+// 派生クラス：Cat
+class Cat : public Animal {
+public:
+    void makeSound() const override {
+        std::cout << "Meow!" << std::endl;
+    }
+};
 
 int main() {
-   
-    srand(time(nullptr));
+    // ポリモーフィズムを使って Animal ポインタで異なるクラスを扱う
+    std::shared_ptr<Animal> myDog = std::make_shared<Dog>();
+    std::shared_ptr<Animal> myCat = std::make_shared<Cat>();
 
-    //サイコロを振る
-    int diceResult = RollDice();
-
-    //出目の奇数偶数を判定
-    int diceParity = diceResult % 2;
-
-    //奇数か偶数かの入力を受け取る
-    int userGuess;
-    printf("奇数(1)か偶数(0)かを当ててください: ");
-    scanf_s("%d", &userGuess);
-
-    //ラムダ式を使った結果表示
-    CheckResult(diceParity, userGuess, [](int isCorrect) {
-        if (isCorrect) {
-            printf("当たりです\n");
-        } else {
-            printf("ハズレです\n");
-        }
-        });
+    myDog->makeSound();  // 出力: Woof!
+    myCat->makeSound();  // 出力: Meow!
 
     return 0;
 }
+
