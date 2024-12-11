62514c305f0333cff14314870d9e442d46d99609
sakuma-shin <k023g0084@g.neec.ac.jp>
2024-12-11 14:37:16 +0900

    課題完了

--- main.cpp
+++ main.cpp
@@ -1,44 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <Windows.h>
+#include <stdio.h>
+#include <iostream>
 
-//サイコロを振る
-int RollDice() {
-    return rand() % 6 + 1;
+// Enemyクラス
+class Enemy {
+public:
+	void Update(); // 更新関数
+
+	void Approach(); // 状態関数1: 接近
+	void Shoot();    // 状態関数2: 射撃
+	void Leave();  // 状態関数3: 離脱
+
+	// メンバ関数ポインタのテーブル
+	static void (Enemy::*stateTable[])();
+
+private:
+	int stateIndex = 0; // 状態を参照するインデックス
+};
+
+void Enemy::Approach() { std::cout << "敵の接近" << std::endl; }
+
+void Enemy::Shoot() { std::cout << "敵の射撃" << std::endl; }
+
+void Enemy::Leave() { std::cout << "敵の離脱" << std::endl; }
+
+void Enemy::Update() {
+	// 現在の状態を実行
+	(this->*stateTable[stateIndex])();
+		// 次の状態に遷移
+		stateIndex = (stateIndex + 1) % 3; // インデックスを循環させる
+	
 }
 
-//結果を判定
-void CheckResult(int result, int guess, void(*callback)(int)) {
-    //三秒間待機
-    Sleep(3000);
-    //呼び出し
-    callback(result == guess);
-}
+// static宣言したメンバ関数ポインタテーブルの実体
+void (Enemy::*Enemy::stateTable[])() = {
+    &Enemy::Approach, // インデックス番号0
+    &Enemy::Shoot,    // インデックス番号1
+    &Enemy::Leave   // インデックス番号2
+};
 
 int main() {
-   
-    srand(time(nullptr));
+	Enemy enemy;
 
-    //サイコロを振る
-    int diceResult = RollDice();
+	// 無限ループで敵の状態を更新
+	for (int i = 0; i < 3; i++) {
+		enemy.Update();
+	}
 
-    //出目の奇数偶数を判定
-    int diceParity = diceResult % 2;
-
-    //奇数か偶数かの入力を受け取る
-    int userGuess;
-    printf("奇数(1)か偶数(0)かを当ててください: ");
-    scanf_s("%d", &userGuess);
-
-    //ラムダ式を使った結果表示
-    CheckResult(diceParity, userGuess, [](int isCorrect) {
-        if (isCorrect) {
-            printf("当たりです\n");
-        } else {
-            printf("ハズレです\n");
-        }
-        });
-
-    return 0;
+	return 0;
 }
