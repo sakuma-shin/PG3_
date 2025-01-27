4ffb8fa343e3f4a2d74c4ccfeea100fc1a20e6a2
sakuma-shin <k023g0084@g.neec.ac.jp>
2025-01-27 10:07:01 +0900

    課題完了

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
-#include<stdio.h>
-#include<Windows.h>
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+mutex mtx;
+condition_variable cv;
+uint32_t current = 1;
+
+void PrintThread(uint32_t num) {
+	unique_lock<mutex> lock(mtx);
+	cv.wait(lock, [num] { return current == num; }); 
+	cout << "thread " << num << endl;
+	++current;
+	cv.notify_all(); 
+}
 
 int main() {
-	SetConsoleOutputCP(65001);
+	thread t1(PrintThread, 1);
+	thread t2(PrintThread, 2);
+	thread t3(PrintThread, 3);
 
-	printf("西畑大吾\n");
+	t1.join();
+	t2.join();
+	t3.join();
+
 	return 0;
 }
